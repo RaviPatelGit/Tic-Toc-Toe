94a1d7e618f8ca50af639a8f01a25258c4def01d
Ravi Patel <hirapra.ravi@gmail.com>
2021-07-03 00:19:11 +0530

    display board

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
 
+let ticArray = ['O','X','O',
+                'X','O','X',
+                'O','X','O'];
+
+
+let board = document.querySelector('.divBoard');
+let a = board.childNodes;
+
+
+
+// const board = () => {
+//     let ticArray = [' ',' ',' ',
+//                     ' ',' ',' ',
+//                     ' ',' ',' '];
+// }
+
+const playButton = document.querySelector('#playButtonId');
+playButton.addEventListener('click', () => {
+    for(let i=1 ; i <=9 ; i++ ){
+        a[2*i-1].innerHTML = "<h1>" + ticArray[i-1] + "</h1>"; 
+    }
+});
+
+
+
+
+
+// board.childNodes.
